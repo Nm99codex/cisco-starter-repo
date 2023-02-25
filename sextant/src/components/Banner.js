db25f06cc873b19403a851d38a16f1ac06dfaa19
Nm99codex <nmcodex@gmail.com>
2023-02-25 15:17:41 +0530

    Add files

--- sextant/src/components/Banner.js
+++ sextant/src/components/Banner.js
@@ -5,8 +5,22 @@ function Banner() {
   return (
     <>
       <div className="m-4 text-[3rem] rounded-lg text-blue-500 font-[900] border-[4px] border-[#00bceb]">
-        <div className=" flex  p-4">
-          <img src={logo} className="w-1/2"/>
+        <div className=" flex p-4 items-center justify-between">
+          <img src={logo} className="w-[4rem]" />
+          <div className=" flex justify-between w-[50%]">
+            <div className="font-serif lg:text-[1rem] mob:text-[0.5rem] hover:underline hover:transition-all hover:delay-150 cursor-pointer">
+              Home
+            </div>
+            <div className="font-serif lg:text-[1rem] mob:text-[0.5rem] hover:underline hover:transition-all hover:delay-150 cursor-pointer">
+              Contact Us
+            </div>
+            <div className="font-serif lg:text-[1rem] mob:text-[0.5rem] hover:underline hover:transition-all hover:delay-150 cursor-pointer">
+              About Us
+            </div>
+            <div className="font-serif lg:text-[1rem] mob:text-[0.5rem] hover:underline hover:transition-all hover:delay-150 cursor-pointer">
+              Our Team
+            </div>
+          </div>
         </div>
       </div>
     </>
--- sextant/tailwind.config.js
+++ sextant/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      mob: "0px",
+      // => @media (min-width: 640px) { ... }
+
+      lg: "640px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
